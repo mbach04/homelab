2fdade11a77449589d39dc1a6ed217131fe04d2c
mbach <mbach@redhat.com>
2022-12-09 21:43:51 -0500

    typo

--- discord-alert/app.py
+++ discord-alert/app.py
@@ -13,14 +13,14 @@ def webhook():
   msg = {"content": payload}
   
   # Set the URL for the Discord webhook
-  webhook_url = os.environment.get('DISCORD_HOOK_URL')
+  webhook_url = os.environ.get('DISCORD_HOOK_URL')
   # Send the response to the Discord webhook
   requests.post(webhook_url, json=msg)
   
   return "OK"
 
 if __name__ == '__main__':
-  port = os.environment.get('FLASK_PORT') or 8080
+  port = os.environ.get('FLASK_PORT') or 8080
   port = int(port)
 
   app.run(port=port,host='0.0.0.0')
