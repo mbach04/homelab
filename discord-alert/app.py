444a822b2e05c7f2def22f7526a41d1a12fbafe6
Matthew Bach <matthewbach@Matthews-MacBook-Pro.local>
2022-12-12 23:38:08 -0500

    ip filter to local cluster network

--- discord-alert/app.py
+++ discord-alert/app.py
@@ -4,18 +4,26 @@ from dhooks import Webhook
 
 app = Flask(__name__)
 
+def is_proper_ip_subnet(client_ip):
+  # Check if the IP address is within the 10.128.0.0/16 network
+  if client_ip.startswith("10.128."):
+    return True
+  return False
 
 @app.route('/1050943624021557278', methods=['POST'])
 def webhook2():
-  webhook_url = os.environ.get('DISCORD_HOOK_URL')
-  hook = Webhook(webhook_url)
-  a = json.dumps(request.json)
-  print("*********raw json received**********")
-  print(request.json)
-  print("**************encoded string************")
-  print(a)
-  hook.send(a)
-  return Response(status=200)
+    # Get the client's IP address
+    if is_proper_ip_subnet(request.remote_addr):
+      webhook_url = os.environ.get('DISCORD_HOOK_URL')
+      hook = Webhook(webhook_url)
+      a = json.dumps(request.json)
+      print("********* raw json received **********")
+      print(request.json)
+      print("************** encoded string ************")
+      print(a)
+      hook.send(a)
+      return Response(status=200)
+    return Response(status=400)
 
 if __name__ == '__main__':
   port = os.environ.get('FLASK_PORT') or 8080
