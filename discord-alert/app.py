3091a10b58c5a14dc0d47b09b0632ad9a8688c8f
Matthew Bach <matthewbach@Matthews-MacBook-Pro.local>
2022-12-13 00:24:37 -0500

    fix dumb

--- discord-alert/app.py
+++ discord-alert/app.py
@@ -21,7 +21,8 @@ def webhook2():
       print("************** discord format ************")
       discord_format = json.dumps({"content": request.json}, indent=4)
       print(discord_format)
-      r = requests.post(webhook_url, json=discord_format)
+      headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+      r = requests.post(webhook_url, json=discord_format, headers=headers)
       if r.ok:
         print("POST returned OK")
       else:
